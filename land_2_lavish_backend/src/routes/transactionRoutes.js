9a0c855ec5275d199e7366e6e0e62a88d71849ab
ShyamSingh841442 <shyamsingh841442@gmail.com>
2025-04-18 16:15:43 +0530

    add allotment backend and its routes

--- land_2_lavish_backend/src/routes/transactionRoutes.js
+++ land_2_lavish_backend/src/routes/transactionRoutes.js
@@ -12,6 +12,19 @@ import {
  } from "../controllers/trasactionController/booking.js";
 
 
+const {
+    createAllotment,
+    getAllotments,
+    getAllotmentById,
+    updateAllotment,
+    deleteAllotment,
+    checkPropertyAllotment
+  } = require("../controllers/trasactionController/allotment.js");
+  
+
+
+
+
  router.post("/booking", createBooking);
  router.get("/booking", getBookings);
  router.get("/booking/:booking_id", getBookingById);
@@ -20,6 +33,14 @@ import {
  router.get("/booking/check-customer-bookings/:customer_id", checkCustomerBookings);
 
 
+ 
+// Allotment routes
+router.post("/allotment", createAllotment);
+router.get("/allotment", getAllotments);
+router.get("/allotment/:allotment_id", getAllotmentById);
+router.put("/allotment/:allotment_id", updateAllotment);
+router.delete("/allotment/:allotment_id", deleteAllotment);
+router.get("/allotment/check-property/:property_id", checkPropertyAllotment);
 
 export default router;
 
